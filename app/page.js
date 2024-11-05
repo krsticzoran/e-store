c2ffc4b521bafb1267167f79721a31d4b765d0cd
zorannkrstic <zorankrstic81@gmail.com>
2024-11-05 10:12:18 +0100

    Tailwind CSS and Sass

--- app/page.js
+++ app/page.js
@@ -34,7 +34,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 bg-black">
           {products.map((product) => (
             <div key={product.id} className="border p-4 rounded-lg">
               {product.images.length > 0 && (
