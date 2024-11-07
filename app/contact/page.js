629cd3fb11e71757922292def1482c5e874ea6dd
zorannkrstic <zorankrstic81@gmail.com>
2024-11-07 10:29:56 +0100

    response from the server

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,14 +1,34 @@
+"use client";
+import { useState } from "react";
 import { sendingEmail } from "@/lib/action";
 
 export default function Contact() {
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const message = await sendingEmail(formData);
+
+    if (message) {
+      setResponseMessage(message);
+    } else {
+      setResponseMessage("Something went wrong. Please try again.");
+    }
+  };
+
   return (
     <>
-      <form action={sendingEmail}>
+      <form onSubmit={handleSubmit}>
         <input type="email" name="email" placeholder="email" />
         <input type="text" name="subject" placeholder="subject" />
         <input type="text" name="message" placeholder="message" />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
+
+      {responseMessage && <p>{responseMessage}</p>}
     </>
   );
 }
