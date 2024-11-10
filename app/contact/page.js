a26740fa488f1ca212631d2fbe1d7fcf0b243b35
zorannkrstic <zorankrstic81@gmail.com>
2024-11-10 10:12:15 +0100

    onFocus

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,19 +3,46 @@
 import { sendingEmail } from "@/lib/action";
 import FormButton from "@/components/form-button";
 import { useFormState } from "react-dom";
+import { useState, useEffect, useRef } from "react";
 
 export default function Contact() {
   const [state, formAction] = useFormState(sendingEmail, { message: "" });
+  const [message, setMessage] = useState("");
+  const ref = useRef(null);
+  useEffect(() => {
+    setMessage(state.message);
+    if (state.message == "Thank you for your message. It has been sent.") {
+      ref.current.reset();
+    }
+  }, [state]);
+  const handleInputFocus = () => {
+    setMessage("");
+  };
 
   return (
     <>
-      <form action={formAction}>
-        <input type="email" name="email" placeholder="email" />
-        <input type="text" name="subject" placeholder="subject" />
-        <input type="text" name="message" placeholder="message" />
+      <form action={formAction} ref={ref}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="text"
+          name="subject"
+          placeholder="subject"
+          onFocus={handleInputFocus}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="message"
+          onFocus={handleInputFocus}
+        />
         <FormButton />
       </form>
-      {state.message}
+      {message && <p>{message}</p>}
     </>
   );
 }
