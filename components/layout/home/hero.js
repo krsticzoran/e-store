db23dc712dc865f2e789ca368676641ca231dffc
zorannkrstic <zorankrstic81@gmail.com>
2024-12-16 09:58:29 +0100

    tablet

--- components/layout/home/hero.js
+++ components/layout/home/hero.js
@@ -9,11 +9,11 @@ export default function Hero() {
   return (
     <section className="bg-body-dark">
       <container className="relative mx-auto flex max-w-[1280px] items-center justify-center overflow-hidden p-5 py-40 xl:p-0 xl:py-40 2xl:max-w-[1440px] 2xl:py-64">
-        <div className="absolute bottom-[100px] left-0 xl:left-[70px] xl:top-[30%] 2xl:left-[50px] 2xl:top-[38%]">
+        <div className="absolute bottom-[100px] left-0 sm:bottom-[20px] sm:left-[5%] lg:left-[10%] xl:left-[70px] xl:top-[30%] 2xl:left-[50px] 2xl:top-[38%]">
           <Image
             src={teaLeaves}
             alt="tea leaves"
-            className="w-[200px] xl:w-[70%] 2xl:w-[84%]"
+            className="w-[200px] sm:w-[320px] lg:w-[360px] xl:w-[70%] 2xl:w-[84%]"
           />
         </div>
         <Image
@@ -24,7 +24,7 @@ export default function Hero() {
         <Image
           src={circle}
           alt="tea leaves"
-          className="absolute w-[90%] xl:w-[60%] 2xl:w-[68%]"
+          className="absolute w-[90%] sm:w-[60%] xl:w-[60%] 2xl:w-[68%]"
         />
         <div>
           <div className="z-0 flex items-center justify-center">
@@ -54,20 +54,21 @@ export default function Hero() {
             </Link>
           </div>
         </div>
-        <div className="absolute bottom-[20px] right-0 xl:-right-[200px] xl:top-1/3 2xl:-right-[150px] 2xl:top-[40%]">
+        <div className="absolute bottom-[20px] right-0 sm:right-[5%] lg:right-[10%] xl:-right-[200px] xl:top-1/3 2xl:-right-[150px] 2xl:top-[40%]">
           <Image
             src={teaKettle}
             priority
             alt="tea kettle"
-            className="w-[200px] xl:w-[70%] 2xl:w-[80%]"
+            className="w-[200px] sm:w-[300px] lg:w-[360px] xl:w-[70%] 2xl:w-[80%]"
           />
         </div>
 
-        <div className="absolute bottom-[50px] left-[32px] flex flex-col items-center justify-center xl:left-[160px]">
+        <div className="absolute bottom-[20px] left-[32px] flex flex-col items-center justify-center xl:bottom-[50px] xl:left-[160px]">
           <Link
             href="https://facebook.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="z-50"
           >
             <i className="fab fa-facebook-f font-medium leading-6 text-[rgba(255,255,255,0.5)] hover:text-secondary"></i>
           </Link>
@@ -75,6 +76,7 @@ export default function Hero() {
             href="https://youtube.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="z-50"
           >
             <i className="fab fa-youtube font-medium leading-6 text-[rgba(255,255,255,0.5)] hover:text-secondary"></i>
           </Link>
@@ -82,6 +84,7 @@ export default function Hero() {
             href="https://linkedin.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="z-50"
           >
             <i className="fab fa-linkedin-in font-medium leading-6 text-[rgba(255,255,255,0.5)] hover:text-secondary"></i>
           </Link>
