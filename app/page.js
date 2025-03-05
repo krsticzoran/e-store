1f09ca6d60ef9cec60abd9b11e10b81c440d7cd0
krsticzoran <zorankrstic81@gmail.com>
2025-03-05 11:35:51 +0100

    product list card

--- app/page.js
+++ app/page.js
@@ -1,19 +1,22 @@
 import ProductList from "@/components/layout/product-list";
 import Hero from "@/components/layout/home/hero";
 import Experience from "@/components/layout/home/experience";
-
-export default function Home() {
+import getProduct from "@/lib/get-products";
+export default async function Home() {
+  const allProducts = await getProduct();
+  const products = allProducts.slice(0, 4);
   return (
     <>
       <Hero />
       <Experience />
       {/*Featured Items */}
-      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-        <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            <ProductList />
-          </div>
-        </main>
+      <div className="mx-auto pb-20 xl:max-w-[1280px]">
+        <h4 className="text-bold mb-6 font-youngSerif text-2xl text-primary">
+          Featured Items
+        </h4>
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
+          <ProductList products={products} />
+        </div>
       </div>
     </>
   );
--- components/layout/product-list.js
+++ components/layout/product-list.js
@@ -1,17 +1,14 @@
-import { stripHtmlTags } from "@/utils/html-utils";
 import Link from "next/link";
 import AddToCartButton from "@/components/ui/add-to-cart-button";
-import getProduct from "@/lib/get-products";
+
+
 import Image from "next/image";
 
-export default async function ProductList() {
-  const products = await getProduct();
-
-  if (products.message) {
-    return <p>{products.message}</p>;
-  }
-
-  console.log(products);
+export default async function ProductList({ products }) {
+  //if (products.message) {
+  //return <p>{products.message}</p>;
+  //}
+  
   return (
     <>
       {products.map((product) => {
@@ -19,8 +16,9 @@ export default async function ProductList() {
         const isOutOfStock = product.stock_status === "outofstock";
         return (
           <div className="relative h-full" key={product.id}>
+             <div>
             <Link href={`/product/${product.id}`}>
-              <div>
+             
                 {product.images.length && (
                   <div className="intems-center flex justify-center bg-warm-beige">
                     <Image
@@ -41,23 +39,46 @@ export default async function ProductList() {
                 )}
                 {hasDiscount && (
                   <div className="absolute right-4 top-4 flex h-16 w-16 items-center justify-center rounded-full bg-[#FDA043] font-bold uppercase text-white">
-                    <p>sale</p>
+                    <p>
+                      -
+                      {Math.floor(
+                        (1 - product.price / product.regular_price) * 100,
+                      )}
+                      %
+                    </p>
                   </div>
                 )}
-
-                {/* Category */}
-
-                {product.categories?.length > 0 && (
-                  <p>{product.categories[0].name}</p>
-                )}
-
+ </Link>
+                {/* product name */}
+                <h5 className="mb-1 mt-6 text-lg font-bold leading-6 text-primary">
+                  {product.name}
+                </h5>
+               
                 {/* Product Info */}
-                <p>{product.price}</p>
-                <p>{product.regular_price}</p>
-                <h2>{product.name}</h2>
-                <p>{stripHtmlTags(product.description)}</p>
+                <div className="flex justify-between">
+                  <div className="flex">
+                    {hasDiscount && (
+                      <p className="mr-2 text-primary text-opacity-40 line-through">
+                        ${product.regular_price}
+                      </p>
+                    )}
+                    <p className="text-primary text-opacity-80">
+                      ${product.price}
+                    </p>
+                  </div>
+                  <Link
+                    href="/cart"
+                    aria-label="View Cart"
+                    className="flex items-center capitalize text-primary text-opacity-90 hover:text-secondary"
+                  >
+                    <p className="mr-3">add to cart</p>
+                    <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+                  </Link>
+                </div>
+
+                {/*<p>{stripHtmlTags(product.description)}</p> pull out html code*/}
               </div>
-            </Link>
+            
           </div>
         );
       })}
