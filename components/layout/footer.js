2cb9efff80c3858a5ffc299aa2495005222dd1aa
krsticzoran <zorankrstic81@gmail.com>
2025-03-06 10:19:52 +0100

    white60

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,5 +1,23 @@
-export default function Footer(){
-    return (
-        <div className="bg-primary h-[500px]"></div>
-    )
+import Image from "next/image";
+import logo from "@/public/icons/logo.svg";
+
+export default function Footer() {
+  return (
+    <div className="h-[500px] bg-primary pt-20">
+      <div className="mx-auto text-white60 xl:max-w-[1280px]">
+        <div className="grid grid-cols-3 gap-10">
+          <div></div>
+          <div className="flex flex-col  items-center">
+            <Image src={logo} width={92} height={93} alt="Company Logo" />
+            <p className="mt-10 text-center">
+              Tea has a complex positive effect on the body. Daily use of a cup
+              of tea is good for your health.
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
