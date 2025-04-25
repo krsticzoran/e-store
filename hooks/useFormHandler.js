8a9837d604b9a5118373304f95412710e44ba213
krsticzoran <zorankrstic81@gmail.com>
2025-04-25 14:50:46 +0200

    form handler

--- hooks/useFormHandler.js
+++ hooks/useFormHandler.js
@@ -1,20 +1,25 @@
-"use client";
 import { useFormState } from "react-dom";
 import { useRef, useEffect, useState } from "react";
 
 export default function useFormHandler(action) {
   const [state, formAction] = useFormState(action, { message: "" });
 
+  // Local state for displaying form messages/errors
   const [message, setMessage] = useState();
+
+  // Ref to access the form DOM element
   const ref = useRef(null);
 
   useEffect(() => {
+    // If submission was successful, reset the form
     if (state.success) {
-      ref.current.reset();
+      ref.current?.reset(); // Safe optional chaining
     }
+    // Update message from form submission response
     setMessage(state.message);
   }, [state]);
 
+  // Clears message when user focuses on an input
   const handleInputFocus = () => {
     setMessage("");
   };
