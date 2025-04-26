d53f8ea5a32d8d88ccbfc2ed1e36ad2fe1f13c9b
krsticzoran <zorankrstic81@gmail.com>
2025-04-26 14:15:13 +0200

    search bar

--- components/shop/productsearchbar.js
+++ components/shop/productsearchbar.js
@@ -29,8 +29,7 @@ export default function ProductSearchBar() {
   }, [searchQuery]);
 
   return (
-    <div className="border border-[#CCC] py-3" role="search">
-        
+    <div className="border border-[#CCC] py-3" role="search"> 
       {/* Search icon - decorative only (hidden from screen readers) */}
       <i
         className="fa-solid fa-magnifying-glass mx-2 text-primary text-opacity-75"
