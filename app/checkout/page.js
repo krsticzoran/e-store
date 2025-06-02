debb47eaa6013f780d4d185bfe69c72d86185bb3
krsticzoran <zorankrstic81@gmail.com>
2025-06-02 12:32:14 +0200

    style: add styles for Shipping Information form

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -4,6 +4,8 @@ import { getNames } from "country-list";
 import { getCartItems, calculateTotal } from "@/utils/cart";
 import Image from "next/image";
 import { submitOrder } from "@/action/submit-order-action";
+import Container from "@/components/ui/container";
+import FormButton from "@/components/ui/form-button";
 
 export default function Checkout() {
   const [countries, setCountries] = useState([]);
@@ -18,79 +20,170 @@ export default function Checkout() {
   }, []);
 
   return (
-    <>
-      <form action={submitOrder.bind(null, cart)}>
-        <h1>Checkout</h1>
-
-        <h3>Contact Information</h3>
-        <p>
-          We&apos;ll use this email to send you details and updates about your
-          order.
-        </p>
-        <input type="email" name="email" placeholder="Email address" required />
-
-        <h3>Shipping Address</h3>
-        <p>Enter the address where you want your order delivered.</p>
-        <select name="country" required>
-          <option value="" disabled>
-            Choose a country
-          </option>
-          {countries.map((name) => (
-            <option key={name} value={name}>
-              {name}
-            </option>
-          ))}
-        </select>
-
-        <input type="text" name="firstName" placeholder="First name" required />
-        <input type="text" name="lastName" placeholder="Last name" required />
-        <input
-          type="text"
-          name="address1"
-          placeholder="Address line 1"
-          required
-        />
-        <input
-          type="text"
-          name="address2"
-          placeholder="Address line 2 (optional)"
-        />
-        <input type="text" name="city" placeholder="City" required />
-        <input
-          type="text"
-          name="state"
-          placeholder="State / Province / Region"
-          required
-        />
-        <input
-          type="text"
-          name="postcode"
-          placeholder="Postcode / ZIP"
-          required
-        />
-        <input type="tel" name="phone" placeholder="Phone number" required />
-
-        <button>Place Order</button>
-      </form>
-      <div>
-        <p>Order summary</p>
-        {cart.map((product) => (
-          <div key={product.id}>
-            <Image
-              src={product.images[0].src}
-              alt={product.name}
-              width={300}
-              height={300}
-              className="rounded-lg"
-            />
-            <p>{product.name}</p>
-            <p>{product.amount}</p>
-            <p> {product.price}</p>
-            <p>{product.amount * product.price}</p>
+    <Container>
+      <div className="mx-auto max-w-[1280px] px-5 pt-20 text-primary lg:flex xl:px-0">
+        <form
+          action={submitOrder.bind(null, cart)}
+          className="basis-2/3 lg:pr-10 xl:pr-20"
+        >
+          <h2 className="mb-5 font-youngSerif text-xl leading-8 md:text-2xl">
+            Shipping Information (required)
+          </h2>
+          <div className="flex gap-x-3">
+            <div className="w-full">
+              <label
+                className="text-primary text-opacity-50"
+                htmlFor="first_name"
+              >
+                First name
+              </label>
+              <input
+                id="first_name"
+                aria-required="true"
+                aria-label="Your first name"
+                type="text"
+                name="firstName"
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                required
+              />
+            </div>
+            <div className="w-full">
+              <label
+                className="text-primary text-opacity-50"
+                htmlFor="last_name"
+              >
+                Last name
+              </label>
+              <input
+                id="last_name"
+                aria-required="true"
+                aria-label="Your last name"
+                type="text"
+                name="lastName"
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                required
+              />
+            </div>
           </div>
-        ))}
-        <p>total : {total}</p>
+          <label className="text-primary text-opacity-50" htmlFor="email">
+            Email address
+          </label>
+          <input
+            id="email"
+            type="email"
+            required
+            className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+            aria-required="true"
+            aria-label="Your email address"
+          />
+          <div className="gap-x-3 md:flex">
+            <div className="w-full">
+              <label className="text-primary text-opacity-50" htmlFor="address">
+                Your street address
+              </label>
+              <input
+                id="address"
+                type="text"
+                name="address1"
+                required
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                aria-required="true"
+                aria-label="Your address"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-primary text-opacity-50" htmlFor="city">
+                Your city
+              </label>
+              <input
+                id="city"
+                type="text"
+                name="city"
+                required
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                aria-required="true"
+                aria-label="Your city"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-primary text-opacity-50" htmlFor="country">
+                Your country
+              </label>
+              <select
+                id="country"
+                name="country"
+                required
+                className="mb-5 w-full appearance-none border bg-white px-5 py-3 outline-none"
+                aria-required="true"
+                aria-label="Your country"
+              >
+                <option value="">Choose a country</option>
+                {countries.map((name) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="flex gap-x-3">
+            <div className="w-full">
+              <label
+                className="text-primary text-opacity-50"
+                htmlFor="postcode"
+              >
+                Your postcode / ZIP
+              </label>
+              <input
+                id="postcode"
+                type="text"
+                name="postcode"
+                required
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                aria-required="true"
+                aria-label="Your postcode"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-primary text-opacity-50" htmlFor="phone">
+                Your phone number
+              </label>
+              <input
+                id="phone"
+                type="tel"
+                name="phone"
+                required
+                className="mb-5 w-full border bg-white px-5 py-3 outline-none"
+                aria-required="true"
+                aria-label="Your phone"
+              />
+            </div>
+          </div>
+          <FormButton className="mt-6 bg-primary px-6 py-4 text-sm font-bold uppercase tracking-[1px] text-white duration-500 hover:bg-secondary">
+            Place Order
+          </FormButton>
+        </form>
+        <div className="basis-1/3 bg-[#F4F4F4]">
+          <p>Order summary</p>
+          {cart.map((product) => (
+            <div key={product.id}>
+              <Image
+                src={product.images[0].src}
+                alt={product.name}
+                width={300}
+                height={300}
+                className="rounded-lg"
+              />
+              <p>{product.name}</p>
+              <p>{product.amount}</p>
+              <p> {product.price}</p>
+              <p>{product.amount * product.price}</p>
+            </div>
+          ))}
+          <p>total : {total}</p>
+        </div>
       </div>
-    </>
+    </Container>
   );
 }
