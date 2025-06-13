2c1da5a98eaf07a705a8d5e96748b53850742bcb
krsticzoran <zorankrstic81@gmail.com>
2025-06-13 15:56:07 +0200

    Improve banner image responsiveness

--- components/ui/banner.js
+++ components/ui/banner.js
@@ -3,9 +3,15 @@ import Image from "next/image";
 
 export default function Banner({ title }) {
   return (
-    <div className="relative h-[200px] w-full lg:h-[250px]">
+    <div className="relative h-[200px] w-full lg:h-[250px] 2xl:h-[300px]">
       {/* Background Image */}
-      <Image src={background} alt="background" fill priority />
+      <Image
+        src={background}
+        alt="background"
+        fill
+        priority
+        className="object-cover"
+      />
       {/* Page Title */}
       <h1 className="absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 font-youngSerif text-5xl capitalize leading-[61px] text-primary">
         {title}
